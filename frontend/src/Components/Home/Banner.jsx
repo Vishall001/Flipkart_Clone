8eff912f223556fbd89edcafa3fbb89340545199
Vishall001 <vshmandloi@gmail.com>
2022-11-28 13:04:37 +0530

    Made the carousel

---
+++ frontend/src/Components/Home/Banner.jsx
@@ -0,0 +1,49 @@
+import styled from "@emotion/styled";
+import React from "react";
+
+import Carousel from "react-multi-carousel";
+import { bannerData } from "../../constants/data";
+import "react-multi-carousel/lib/styles.css";
+
+const Image = styled("img")({
+  width: "100%",
+  height: 280,
+});
+
+const Banner = () => {
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 1,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 1,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+
+  return (
+    <Carousel
+      responsive={responsive}
+      dotListClass="custom-dot-list-style"
+      itemClass="carousel-item-padding-40-px"
+      containerClass="carousel-container"
+      swipeable={false}
+      draggable={false}
+      infinite={true}
+      autoPlay={true}
+      autoPlaySpeed={4000}
+      slidesToSlide={1}
+    >
+      {bannerData.map((data) => {
+        return <Image src={data.url} alt="Banner" />;
+      })}
+    </Carousel>
+  );
+};
+
+export default Banner;
--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import Navbar from './Navbar'
+import styled from "@emotion/styled";
+import { Box } from "@mui/system";
+import React from "react";
+import Banner from "./Banner";
+import Navbar from "./Navbar";
+
+const Container = styled(Box)`
+  padding: 10px;
+  background: #f2f2f2;
+`;
 
 const Home = () => {
   return (
-       <Navbar/>
-  )
-}
+    <>
+      <Navbar />
+      <Container>
+        <Banner />
+      </Container>
+    </>
+  );
+};
 
-export default Home
+export default Home;
